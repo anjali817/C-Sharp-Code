9cbfb01ff8e6e219850da94866da2f87757cc562
anjali817 <99798215+anjali817@users.noreply.github.com>
2022-02-17 13:45:47 +0530

    Add files via upload
    Table Printing Code

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class csharpExercise
+{
+    static void Main(string[] args)
+    {
+
+        int i, num;
+
+        //Reading number
+        Console.WriteLine("Enter number to print table: ");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        for (i = 1; i <= 10; i++)
+        {
+            //Printing table of number entered by user            
+            Console.WriteLine(+ num * i);
+        }
+        Console.ReadLine();
+    }
+}
