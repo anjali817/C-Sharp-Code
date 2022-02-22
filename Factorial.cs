64ec40398fe1a93cac0e9eb1a5c7b4ef9b9a4375
anjali817 <99798215+anjali817@users.noreply.github.com>
2022-02-23 00:34:18 +0530

    Factorial

---
+++ Factorial.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more informatio
+using System;
+public class Factorial
+{
+    public static void Main(String[] args)
+    {
+        int i, fact = 1, n;
+        Console.WriteLine("Enter Any Number");
+        n = int.Parse(Console.ReadLine());
+        for (i =1; i <= n; i++)
+        {
+            fact = fact * i;
+
+        }
+        Console.WriteLine("Factorial of"+n+"is"+fact);
+    }
+}
