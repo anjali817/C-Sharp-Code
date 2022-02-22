6c90ca639fb6fe619ef6afa27155edd3a65cadb1
anjali817 <99798215+anjali817@users.noreply.github.com>
2022-02-23 00:35:06 +0530

    Star Pattern 1

---
+++ Star Pattern 1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace MyApplication
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            for (int row = 1; row <= 5; ++row)
+            {
+                for (int col = 1; col <= row; ++col)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
