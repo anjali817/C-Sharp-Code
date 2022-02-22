0c9db0e1ce21c4bd7c214b1c1632c7e6491da874
anjali817 <99798215+anjali817@users.noreply.github.com>
2022-02-23 00:35:44 +0530

    Sum of 10 number

---
+++ Sum of first 10.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Program
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var sum = 0;
+            for (var i = 1; i <= 10; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine($"This sum is :- { sum}");
+        }
+    }
+}
